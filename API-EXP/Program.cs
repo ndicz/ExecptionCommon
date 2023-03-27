942cacbdcdedda046cc7126d9a50490ef2c100a6
ndicz <hnrmanesi@gmail.com>
2023-03-27 16:49:39 +0700

    API Test

---
+++ API-EXP/Program.cs
@@ -0,0 +1,48 @@
+using R_APIStartUp;
+
+var loBuilder = WebApplication.CreateBuilder(args);
+loBuilder
+    .R_RegisterServices(builder =>
+    {
+        builder.R_DisableAuthentication();
+        //builder.R_DisableSwagger();
+        //builder.R_DisableGlobalException();
+        builder.R_DisableContext();
+        builder.R_DisableDatabase();
+    })
+    .Build()
+    .R_SetupMiddleware()
+    .Run();
+
+
+
+
+
+
+
+/*
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+*/
+
